b3e74789a58c4bab7e2abcfd1eb39bc799bd7a7b
CianEri <cianjustado.gmail.com>
2022-09-28 20:02:08 +0800

    28/9/22

--- script.js
+++ script.js
@@ -83,3 +83,14 @@ function newElement(){
         }
     }
 }
+var codingProjects = document.querySelectorAll('.project');
+
+codingProjects.forEach( (project, index) => {
+    project.dataset.aos = "fade-down"
+    project.dataset.aosDuration = 1500;
+    //project.dataset.aosDelay = index * 300
+});
+
+AOS.init({
+    once: true
+});
